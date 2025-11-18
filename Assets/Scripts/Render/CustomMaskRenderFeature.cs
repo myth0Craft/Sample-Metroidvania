18a6c535691c8e543da2f7cc91db8c98096b4a02
Owen Plett <owen.plett@gmail.com>
2025-11-17 18:12:08 -0700

    finally got foreground blur working (sort of). only one blur layer renders at a time.

--- Assets/Scripts/Render/CustomMaskRenderFeature.cs
+++ Assets/Scripts/Render/CustomMaskRenderFeature.cs
@@ -11,17 +11,22 @@ public class CustomMaskRenderFeature : ScriptableRendererFeature
     [SerializeField] CustomMaskRenderFeatureSettings settings;
     [SerializeField] RenderPassEvent pass;
     CustomMaskRenderFeaturePass m_ScriptablePass;
-
+    private RTHandle handle;
     /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new CustomMaskRenderFeaturePass(settings);
+        handle = RTHandles.Alloc(
+        settings.tex
+        );
+
+        m_ScriptablePass = new CustomMaskRenderFeaturePass(settings, handle);
 
         if (pass != null) {
             m_ScriptablePass.renderPassEvent = pass;
         } else 
             m_ScriptablePass.renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
 
+        
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
@@ -34,23 +39,27 @@ public class CustomMaskRenderFeature : ScriptableRendererFeature
     {
         public Material material;
         public RenderTexture tex;
+        public float blurAmount;
         
 
     }
 
     class CustomMaskRenderFeaturePass : ScriptableRenderPass
     {
+        private readonly RTHandle handle;
         readonly CustomMaskRenderFeatureSettings settings;
 
-        public CustomMaskRenderFeaturePass(CustomMaskRenderFeatureSettings settings)
+        public CustomMaskRenderFeaturePass(CustomMaskRenderFeatureSettings settings, RTHandle handle)
         {
             this.settings = settings;
+            this.handle = handle;
         }
 
         class PassData
         {
             public TextureHandle source;
             public Material blitMaterial;
+            public float blurAmount;
         }
 
 
@@ -64,27 +73,27 @@ public class CustomMaskRenderFeature : ScriptableRendererFeature
             var resourceData = frameData.Get<UniversalResourceData>();
             var cameraColor = resourceData.activeColorTexture;
             RTHandle rt = RTHandles.Alloc(settings.tex);
-            TextureHandle sourceHandle = renderGraph.ImportTexture(rt);
+            TextureHandle sourceHandle = renderGraph.ImportTexture(handle);
             using (var builder = renderGraph.AddRasterRenderPass<PassData>("Blur Blit Pass", out var passData))
             {
                 builder.SetRenderAttachment(cameraColor, 0);
 
                 passData.source = sourceHandle;
                 passData.blitMaterial = settings.material;
+                passData.blurAmount = settings.blurAmount;
 
                 builder.UseTexture(passData.source);
-
+                
 
 
                 builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
                 {
-
+                    
                     data.blitMaterial.SetTexture("_MainTex", data.source);
-                    data.blitMaterial.SetFloat("_BlurAmount", 0.000f);
+                    data.blitMaterial.SetFloat("_BlurAmount", data.blurAmount);
                     Vector2 viewportScale = rt.useScaling ? new Vector2(rt.rtHandleProperties.rtHandleScale.x, rt.rtHandleProperties.rtHandleScale.y) : Vector2.one;
-                    Blitter.BlitTexture2D(context.cmd, data.source, viewportScale, 0, true);
-                    //Blitter.BlitTexture(context.cmd, data.source, viewportScale, data.blitMaterial, 0);
-
+                    //Blitter.BlitTexture2D(context.cmd, data.source, viewportScale, 0, true);
+                    Blitter.BlitTexture(context.cmd, data.source, viewportScale, data.blitMaterial, 0);
 
                 });
                 
