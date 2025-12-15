47a6e8742ec44517455361ece50cd0c75c245687
Owen Plett <owen.plett@gmail.com>
2025-12-14 21:45:32 -0700

    added bush break particles

--- Assets/Scripts/Object/BreakableObject.cs
+++ Assets/Scripts/Object/BreakableObject.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class BreakableObject : HealthManager
 {
 
+    public GameObject breakParticlesPrefab;
+
     public override void Die()
     {
         print("object broken");
         Destroy(this.gameObject);
+
+        Instantiate(
+            breakParticlesPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+
+        Destroy(gameObject);
+
     }
 }
